84bac3a6cc56bc46cefc3c744cfe74de9edf30f7
Thanh Vinh Nguyen <ngtv0404@gmail.com>
2024-01-29 21:44:37 +0700

    Modified DriveTeleOp to use RR

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTeleOp.java
@@ -1,21 +1,25 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.acmerobotics.roadrunner.Pose2d;
+
+import com.acmerobotics.roadrunner.PoseVelocity2d;
+import com.acmerobotics.roadrunner.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import org.firstinspires.ftc.teamcode.Drivetrain.MecanumDrivetrain;
 import org.firstinspires.ftc.teamcode.Mechanisms.Launcher;
 import org.firstinspires.ftc.teamcode.Mechanisms.Slide;
 
 @TeleOp(name = "Driving TeleOp")
 public class DriveTeleOp extends LinearOpMode {
-    private MecanumDrivetrain drivetrain;
+    private MecanumDrive drivetrain;
+
     private Launcher launcher;
     private Slide slide;
 
     @Override
     public void runOpMode() {
-        drivetrain = new MecanumDrivetrain(hardwareMap, telemetry);
+        drivetrain = new MecanumDrive(hardwareMap, new Pose2d(0, 0, 0));
         launcher = new Launcher(hardwareMap);
         slide = new Slide(hardwareMap);
 
@@ -25,8 +29,19 @@ public class DriveTeleOp extends LinearOpMode {
         waitForStart();
 
         while(opModeIsActive()) {
-            drivetrain.updatePosition();
-            drivetrain.drive(gamepad1.left_stick_x, -gamepad1.left_stick_y, gamepad1.right_stick_x); // NOTE: top joystick positions result in negative Y reading so we need to negate it
+            drivetrain.setDrivePowers(new PoseVelocity2d(
+                    new Vector2d(
+                            -gamepad1.left_stick_y,
+                            -gamepad1.left_stick_x
+                    ),
+                    -gamepad1.right_stick_x
+            ));
+            drivetrain.updatePoseEstimate();
+
+            telemetry.addData("x", drivetrain.pose.position.x);
+            telemetry.addData("y", drivetrain.pose.position.y);
+            telemetry.addData("Heading", Math.toDegrees(drivetrain.pose.heading.toDouble()));
+
             launcher.setState(gamepad1.options);
             slide.setState(gamepad1.cross);
             telemetry.update();
